e79758466b77af5bc50175b4d9090bd11b7c19e8
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-06-08 08:24:48 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,10 +3,10 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "mickeyruni18@mail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$headers = "From: noreply@mayankrunija.github.io";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
